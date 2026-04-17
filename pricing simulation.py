22cee5402437b6036a90b632f99921077c135c59
Yeni Jung <yenij0178@gmail.com>
2026-04-17 19:22:37 +0100

    result analysis added further.

--- pricing simulation.py
+++ pricing simulation.py
@@ -46,20 +46,28 @@ print("Loss group mean:")
 print(loss_df[['age', 'bmi', 'predicted_cost', 'charges', 'profit']].mean())
 print("\nOverall mean:")
 print(df_model[['age', 'bmi', 'predicted_cost', 'charges', 'profit']].mean())
+# result: actual doubled predicted. -> not about margin but model prediction error.
 
 print("Loss rate by customer:")
 print(loss_df['smoker'].value_counts(normalize=True))
 print(df_model.groupby('smoker')['profit'].apply(lambda x: (x < 0).mean()))
 print("Loss rate by BMI:")
 print(df_model.groupby('bmi_30')['profit'].apply(lambda x: (x < 0).mean()))
+# result: most of the losses are non-smoker cases. bmi does not have much differences.
 
 print("Underpricing check:")
 print(loss_df[['charges', 'predicted_cost', 'premium', 'profit']].head(10))
+# result: charges (C) = 30k, predicted (C^)= 14k, premium = 16k, profit = -14k
+# -> applying these values into the profit and premium formulas, this means it is model prediction accuracy problem not margin.
 
 print("Check with residual:")
 loss_df['residual'] = loss_df['charges'] - loss_df['predicted_cost']
 print(loss_df['residual'].describe())
+# result: mean residual: about 12300 whereas maximum residual: about 24300 -> too large
 
 print("Check the high-cost section intensively:")
 high_cost_loss = loss_df[loss_df['charges'] > 30000]
 print("High-cost loss ratio:", len(high_cost_loss) / len(loss_df))
+# result: ratio about 20%
+
+## To conclude, models operate poorly with non-smoker high-cost prediction and unexpected medical cost.
